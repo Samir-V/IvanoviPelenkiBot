9eb5d66429dd31da0cc384acec40267bd57f41bf
Samir-V <161743311+Samir-V@users.noreply.github.com>
2026-04-06 22:10:24 +0200

    Fix Port issue

--- IvanoviPelenkiBot.py
+++ IvanoviPelenkiBot.py
@@ -715,6 +715,13 @@ def get_tasks_keyboard(level):
 
 # --- ЗАПУСК ---
 async def main():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    print(f"Web server running on port {port}")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
